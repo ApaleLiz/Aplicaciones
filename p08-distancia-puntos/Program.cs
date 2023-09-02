567d069ef860733197e73e642227e8d93763468c
LizApale <bethapale30@gmail.com>
2023-09-01 21:25:16 -0600

    Tarea1

---
+++ p08-distancia-puntos/Program.cs
@@ -0,0 +1,29 @@
+﻿//Calcular la distancia entre dos puntos, dadas las coodenadas del punto A y el punto B
+
+Console.WriteLine("Calcular la distancia entre dos puntos, dadas las coodenadas del punto A y el punto B\n");
+Console.Clear();
+
+            Console.WriteLine("Ingrese las coordenadas del punto A:");
+            Console.Write("ax: ");
+            double ax = Convert.ToDouble(Console.ReadLine());
+            Console.Write("ay: ");
+            double ay = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Ingrese las coordenadas del punto B:");
+            Console.Write("bx: ");
+            double bx = Convert.ToDouble(Console.ReadLine());
+            Console.Write("by: ");
+            double by = Convert.ToDouble(Console.ReadLine());
+
+            double distancia = CalcularDistancia(ax, ay, bx, by);
+
+            Console.WriteLine($"La distancia entre A y B es: {distancia:f2}");
+
+        static double CalcularDistancia(double ax, double ay, double bx, double by)
+        {
+            double diferenciaX = ax - bx;
+            double diferenciaY = ay - by;
+            double distancia = Math.Sqrt(Math.Pow(diferenciaX, 2) + Math.Pow(diferenciaY, 2));
+            return distancia;
+        }
+    
---
+++ p09-volumen-esfera/Program.cs
@@ -0,0 +1,18 @@
+﻿//Dado el radio de un círculo, calcula el volumen de la esfera
+
+ Console.Clear();
+ Console.WriteLine("Dado el radio de un círculo, calcula el volumen de la esfera\n");
+ Console.Write("Ingrese el radio: ");
+    double radio = Convert.ToDouble(Console.ReadLine());
+
+    double volumen = CalcularVolumenEsfera(radio);
+    Console.WriteLine($"El volumen de la esfera con radio {radio} es: {volumen:f2}");
+
+        Console.ReadLine();
+        
+
+    static double CalcularVolumenEsfera(double radio)
+    {
+        double volumen = (4.0 / 3.0) * Math.PI * Math.Pow(radio, 3);
+        return volumen;
+    }
